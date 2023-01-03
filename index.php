0296428d7f2baf54ff0f7eeb1d1e93e594dfb952
sorwartalukder <mdsorwar4039@gmail.com>
2023-01-03 07:46:37 +0600

    first commit

---
+++ index.php
@@ -0,0 +1,34 @@
+<!-- <?php echo 'hello sorwar'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+<h1><?php echo 'Hello'; ?> PHP</h1>
+<?php
+echo "Sorwar Talukder ";
+echo "Sorwar Talukder";
+?>
+</body>
+</html> -->
+
+
+<!-- <?php
+$name = "sorwar";
+$price=150;
+$address='test ara, test, test';
+$text='Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit ipsa optio assumenda molestias facilis accusamus quae ab, sapiente consequatur quia, voluptates porro impedit ut corrupti iusto repudiandae tenetur obcaecati possimus.';
+$isAdmin =true;
+echo "Name: $name, Address: $address ";
+echo 'Hello my name '.$name .', your Total Amount is: '.$price;
+?> -->
+
+<!-- <?php
+$sorwar='bariani khai';
+$name = "sorwar";
+echo $$name
+?> -->
